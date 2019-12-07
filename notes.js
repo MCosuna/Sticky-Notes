7e51b20f314865fe60d4680a70edc2eb4bd55425
Maria del carmen Osuna <osunaojeda@gmail.com>
2019-12-07 19:03:54 +0100

    first commit for Sticky Notes

---
+++ notes.js
@@ -0,0 +1,11 @@
+class Notes {
+  constructor(width, height, color, texto) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.texto = texto;
+
+    this.nota = createElement("textArea");
+    // this.nota.style.
+  }
+}
